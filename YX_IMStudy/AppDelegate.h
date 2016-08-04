b4e1a508d799188bec9d25f5d2aad70615c3caaf
populary <410445919@qq.com>
2016-08-04 23:49:10 +0800

    Initial Commit

---
+++ YX_IMStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YX_IMStudy
+//
+//  Created by yang on 16/8/4.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YX_IMStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YX_IMStudy
+//
+//  Created by yang on 16/8/4.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
